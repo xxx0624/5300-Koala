2029b9c37f4a81b99cb230fe170bf6517f346dff
martinezxavi <martinezxavi@seattleu.edu>
2020-04-19 00:08:16 -0700

    updated sql5300 and makefile

--- sql5300.cpp
+++ sql5300.cpp
@@ -2,11 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include "db_cxx.h"
-/*
- *@file sql5300.cpp main entry for the sql5300 relational db manager.
+#include "SQLParser.h"
+
+/*Milestone 1
+ * Koala 4/18/2020
  */
 using namespace std;
+using namespace hsql;
 
+string executeCreate(const CreateStatement *stmt){
+	string ret("CREATE TABLE ");
+	if(stmt->type != CreateStatement::kTable)
+		return ret + "...";
+	if(stmt->ifNotExists)
+		ret +="IF NOT EXISTS ";
+	ret += string(stmt->tableName) + "(...)";
+	//FIXME get columns
+	return ret; 	
+}
+
+//String execute for SQL statements
+string execute(const SQLStatement* stmt){
+	switch (stmt->type()){
+	case kStmtSelect:
+		return "SELECT ...";
+	case kStmtCreate:
+		return executeCreate((const CreateStatement *)stmt);
+	default:
+		return "Not implemented.";
+	}
+}
 int main(int argc, char *argv[]) {
 
 	if(argc != 2){
@@ -21,19 +46,51 @@ int main(int argc, char *argv[]) {
 	env.set_error_stream(&std::cerr);
 	env.open(envHome, DB_CREATE | DB_INIT_MPOOL, 0);
 
+	try {
+		env.open(envHome, DB_CREATE | DB_INIT_MPOOL, 0);
+	}
+	catch (DbException & exc) {
+		cerr << "(sql5300: " << exc.what() << ")" << endl;
+		return 1; 
+	}
+
 	while(true) {
 	cout << "SQL> ";
 	string query;
 	getline(cin,query);
+
 	if(query.length() == 0)
 	  continue;
 	if(query == "quit")
 	  break;
-
-	cout << "Query: "<< query << endl;
+	
+	SQLParserResult* result = SQLParser::parseSQLString(query);
+	if(!result->isValid()){
+		cout << "Invalid SQL: " << query << endl; 
+		delete result; 
+		continue;
+	} else {
+		cout << "Valid" << endl; 
+	}
+	
+	for(int i = 0; i < result->size(); i++){
+		cout << execute(result->getStatement(i)) << endl;
+	}
+	delete result;
 	}
 		
 	
 	return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
+
+
+
+
 
